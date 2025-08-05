961cd35dc40c67879dcad9f9ce69366421d7dc7e
EmmaR <emmarawlingswastaken@gmail.com>
2025-08-05 18:41:03 +0100

    Add mod menu scripts

--- diffops_installer_macos.sh
+++ diffops_installer_macos.sh
@@ -1,4 +1,8 @@
 echo "Installing difficulty options for all chapters..."
+./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter1_mac/game.ios --scripts diffops_installfiles\Scripts\modmenu_ch1to4.csx --verbose false --output chapter1_mac/game.ios
+./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter2_mac/game.ios --scripts diffops_installfiles\Scripts\modmenu_ch1to4.csx --verbose false --output chapter2_mac/game.ios
+./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter3_mac/game.ios --scripts diffops_installfiles\Scripts\modmenu_ch1to4.csx --verbose false --output chapter3_mac/game.ios
+./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter4_mac/game.ios --scripts diffops_installfiles\Scripts\modmenu_ch1to4.csx --verbose false --output chapter4_mac/game.ios
 ./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter1_mac/game.ios --scripts diffops_installfiles/Scripts/diffops_ch1.csx --verbose false --output chapter1_mac/game.ios
 ./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter2_mac/game.ios --scripts diffops_installfiles/Scripts/diffops_ch2.csx --verbose false --output chapter2_mac/game.ios
 ./diffops_installfiles/UTMT_CLI_v0.8.3.0-macOS/UndertaleModCli load chapter3_mac/game.ios --scripts diffops_installfiles/Scripts/diffops_ch3.csx --verbose false --output chapter3_mac/game.ios
