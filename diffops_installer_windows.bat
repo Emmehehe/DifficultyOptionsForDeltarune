ac1eb9816927c5f2ecf1790f3bd5a34246bd57c8
EmmaR <emmarawlingswastaken@gmail.com>
2025-08-05 18:39:41 +0100

    Fix installer

--- diffops_installer_windows.bat
+++ diffops_installer_windows.bat
@@ -1,8 +1,12 @@
 @echo off
 echo Installing difficulty options for all chapters...
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx,diffops_installfiles\Scripts\diffops_ch1.csx" --verbose false --output "chapter1_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx,diffops_installfiles\Scripts\diffops_ch2.csx" --verbose false --output "chapter2_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx,diffops_installfiles\Scripts\diffops_ch3.csx" --verbose false --output "chapter3_windows\data.win"
-"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx,diffops_installfiles\Scripts\diffops_ch4.csx" --verbose false --output "chapter4_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter1_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter2_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter3_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "diffops_installfiles\Scripts\modmenu_ch1to4.csx" --verbose false --output "chapter4_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter1_windows\data.win" --scripts "diffops_installfiles\Scripts\diffops_ch1.csx" --verbose false --output "chapter1_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter2_windows\data.win" --scripts "diffops_installfiles\Scripts\diffops_ch2.csx" --verbose false --output "chapter2_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter3_windows\data.win" --scripts "diffops_installfiles\Scripts\diffops_ch3.csx" --verbose false --output "chapter3_windows\data.win"
+"diffops_installfiles\UTMT_CLI_v0.8.3.0-Windows\UndertaleModCli.exe" load "chapter4_windows\data.win" --scripts "diffops_installfiles\Scripts\diffops_ch4.csx" --verbose false --output "chapter4_windows\data.win"
 echo Finished installation.
 pause
